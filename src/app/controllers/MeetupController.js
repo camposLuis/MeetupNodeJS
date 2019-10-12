b2df2a89caab8d0269cfffd13fb69265177b5d41
camposLuis <luis.campos.henrique@hotmail.com>
2019-10-12 15:39:17 -0300

    Finalizando cadastro de meetups

--- src/app/controllers/MeetupController.js
+++ src/app/controllers/MeetupController.js
@@ -1,10 +1,32 @@
-// import * as Yup from 'yup';
+import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import Meetup from '../models/Meetup';
 // import User from '../models/User';
 // import File from '../models/File';
 
 class MeetupController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      title: Yup.string().required(),
+      description: Yup.string().required(),
+      location: Yup.string().required(),
+      date: Yup.date().required(),
+      organizer_id: Yup.number().required(),
+      banner_id: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Validation fails' });
+    }
+
+    const { date } = req.body;
+
+    const meetupDateStart = startOfHour(parseISO(date));
+
+    if (isBefore(meetupDateStart, new Date())) {
+      return res.status(400).json({ error: 'Past dates are not permitted' });
+    }
+
     const meetup = await Meetup.create(req.body);
 
     return res.json(meetup);
