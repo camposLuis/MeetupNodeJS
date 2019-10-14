482afa87c6495458e1603407809f631daf30b637
camposLuis <luis.campos.henrique@hotmail.com>
2019-10-14 00:12:19 -0300

    Enviando email de inscrição

--- src/app/controllers/SubscriptionController.js
+++ src/app/controllers/SubscriptionController.js
@@ -1,6 +1,11 @@
+import { format } from 'date-fns';
+import pt from 'date-fns/locale/pt';
+
 import Subscription from '../models/Subscription';
 import Meetup from '../models/Meetup';
 import User from '../models/User';
+
+import Mail from '../../lib/Mail';
 
 class SubscriptionController {
   async index(req, res) {
@@ -33,7 +38,15 @@ class SubscriptionController {
   }
 
   async store(req, res) {
-    const meetup = await Meetup.findByPk(req.params.meetupId);
+    const meetup = await Meetup.findByPk(req.params.meetupId, {
+      include: [
+        {
+          model: User,
+          as: 'organizer',
+          attributes: ['name', 'email'],
+        },
+      ],
+    });
 
     if (!meetup) {
       return res.status(400).json({ error: 'Meetup does not exist' });
@@ -82,6 +95,26 @@ class SubscriptionController {
     const subscription = await Subscription.create({
       participant_id: req.userId,
       meetup_id: req.params.meetupId,
+    });
+
+    const registered = await User.findByPk(req.userId, {
+      attributes: ['name', 'email'],
+    });
+
+    await Mail.sendMail({
+      to: `${meetup.organizer.name} <${meetup.organizer.email}>`,
+      subject: 'Inscrição confirmada',
+      template: 'subscription',
+      context: {
+        organizer: meetup.organizer.name,
+        participant: registered.name,
+        email: registered.email,
+        title: meetup.title,
+        date: format(meetup.date, "'dia'  dd 'de' MMMM' às' H:mm'h'", {
+          locale: pt,
+        }),
+        location: meetup.location,
+      },
     });
 
     return res.json(subscription);
